43204429e1faec7bcda52eed22b5ba34e5692963
Maksim Tropnikov <makstropnikov@yandex.ru>
2021-06-01 03:41:03 +0700

    fix: fix popup size on mobile and clean up code

--- scripts/main.js
+++ scripts/main.js
@@ -16,32 +16,18 @@ function togglePopupClass() {
 //   }
 // });
 
-// Находим форму в DOM
 let formElement = document.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
 let nameInput = formElement.querySelector("[name='profile-name']"); // Воспользуйтесь инструментом .querySelector()
 let jobInput = formElement.querySelector("[name='profile-bio']"); // Воспользуйтесь инструментом .querySelector()
-// console.log(jobInput);
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-
-  // Получите значение полей jobInput и nameInput из свойства value
   let nameInputValue = nameInput.value;
   let jobInputValue = jobInput.value;
-
-  // Выберите элементы, куда должны быть вставлены значения полей
   let profileName = document.querySelector(".profile__name");
   let profileBio = document.querySelector(".profile__bio");
   profileName.textContent = nameInputValue;
   profileBio.textContent = jobInputValue;
-
-  // Вставьте новые значения с помощью textContent
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler);
