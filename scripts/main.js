a1fb51a6b292a393667692f4856c63c0aea5663c
Maksim Tropnikov <makstropnikov@yandex.ru>
2021-06-04 01:03:38 +0700

    fix: fixed remarks after code review

--- scripts/main.js
+++ scripts/main.js
@@ -26,9 +26,6 @@ function closePopup() {
 
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // let nameInputValue = nameInput.value;
-  // let jobInputValue = jobInput.value;
-  let profileName = document.querySelector('.profile__name');
   let profileBio = document.querySelector('.profile__bio');
   profileName.textContent = nameInput.value;
   profileBio.textContent = jobInput.value;
