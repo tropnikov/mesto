d0dfa04698da731b902baba8b30d0ec8fd9ada88
Maksim Tropnikov <makstropnikov@yandex.ru>
2021-07-29 19:16:06 +0700

    feat: add webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 import {
   initialCards,
   validationConfig,
@@ -13,14 +15,14 @@ import {
   bioInput,
   nameInput,
   userData,
-} from './constants.js';
+} from '../scripts/constants.js';
 
-import FormValidator from './FormValidator.js';
-import Section from './Section.js';
-import Card from './Card.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
+import FormValidator from '../scripts/FormValidator.js';
+import Section from '../scripts/Section.js';
+import Card from '../scripts/Card.js';
+import PopupWithImage from '../scripts/PopupWithImage.js';
+import PopupWithForm from '../scripts/PopupWithForm.js';
+import UserInfo from '../scripts/UserInfo.js';
 
 const userInfo = new UserInfo(userData);
 
