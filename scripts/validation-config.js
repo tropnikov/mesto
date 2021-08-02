2657189d4b433cb60f4ccb75216b5d14f2943545
Maksim Tropnikov <makstropnikov@gmail.com>
2021-08-02 22:38:20 +0700

    Updates

--- scripts/validation-config.js
+++
@@ -1,8 +0,0 @@
-export const validationConfig = {
-  formSelector: '.form',
-  inputSelector: '.form__input',
-  submitButtonSelector: '.form__submit',
-  inactiveButtonClass: 'button_disabled',
-  inputErrorClass: 'form__input_type_error',
-  errorClass: 'form__input-error_active',
-};
